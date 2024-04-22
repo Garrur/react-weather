a4627468a353a8780abb70b099a447bc0628ebaa
Utkarsh Raj <uraj33175@gmail.com>
2024-04-22 12:53:04 +0530

    updated env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 // src/env.d.ts
 interface ImportMeta {
     readonly env: {
-      readonly WETAPI_Key: string;
+      readonly REACT_APP_API_KEY: string;
       // Add other environment variables as needed
     };
   }
